a78ca41aadce5451c463c8cdc22000c3c2482143
RubenRidderstrom <RubenRidderstrom@users.noreply.github.com>
2018-08-17 10:47:31 +0200

    Removed missed debugging code

--- modelestimator/modelestimator.py
+++ modelestimator/modelestimator.py
@@ -1,10 +1,10 @@
 import sys
 import numpy as np
-from ._calculate_q_eq.calculate_q_eq import calculate_q_eq
 from ._handle_input.handle_input_file import handle_input_file
+from ._calculate_q_eq.calculate_q_eq import calculate_q_eq
 
 def modelestimator(FILE_PATH):
     SEQUENCE_LIST = handle_input_file(FILE_PATH)
     Q, EQ = calculate_q_eq(SEQUENCE_LIST)
-    print(__name__)
+    
     return Q, EQ
