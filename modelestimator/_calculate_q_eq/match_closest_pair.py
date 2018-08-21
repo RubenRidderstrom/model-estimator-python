513c1268409e5a8d6cba8702a70a5cccb4452f72
RubenRidderstrom <RubenRidderstrom@users.noreply.github.com>
2018-08-21 17:02:28 +0200

    Made match_closest_pair to improve bottleneck

--- modelestimator/_calculate_q_eq/match_closest_pair.py
+++ modelestimator/_calculate_q_eq/match_closest_pair.py
@@ -2,19 +2,25 @@
 def _matching_letters(a,b, COMPARE_INDELS_FLAG):
     assert(len(a) == len(b))
 
+    number_of_matching_positions = 0
+
     if COMPARE_INDELS_FLAG:
-        NUMBER_OF_MATCHING_LETTERS = sum ((a[i] == b[i] and a[i] != "-") for i in range(len(a)) )
-    else:        
-        NUMBER_OF_MATCHING_LETTERS = sum ((a[i] == b[i]) for i in range(len(a)) ) 
+        for x,y in zip(a,b):
+            if x == y and x != '-' :
+                number_of_matching_positions += 1
+    else:
+        for x,y in zip(a,b):
+            if x==y:
+                number_of_matching_positions += 1
            
-    return NUMBER_OF_MATCHING_LETTERS
+    return number_of_matching_positions
 
 ### Interface
 def match_closest_pairs(sequence_list, COMPARE_INDELS_FLAG):
     assert (len(sequence_list) % 2) == 0
 
     closest_pairs = []
-        
+
     # Compares every sequence to every other sequence
     # Puts the two most similar sequences in closest_pairs and then starts over
     while(len(sequence_list) != 0):
@@ -22,13 +28,14 @@ def match_closest_pairs(sequence_list, COMPARE_INDELS_FLAG):
         max_matching_pair_index = ()
     
         for PRIMARY_INDEX, PRIMARY_SEQUENCE in enumerate(sequence_list):
-            for SECONDARY_INDEX, SECONDARY_SEQUENCE in enumerate(sequence_list):
-                if PRIMARY_INDEX != SECONDARY_INDEX:
-                    MATCHING_LETTERS = _matching_letters(PRIMARY_SEQUENCE, SECONDARY_SEQUENCE, COMPARE_INDELS_FLAG)
-                    if MATCHING_LETTERS > max_matching_letters:
-                        max_matching_letters = MATCHING_LETTERS
-                        max_matching_pair_index = (PRIMARY_INDEX, SECONDARY_INDEX)
-        
+            for SECONDARY_INDEX in range (PRIMARY_INDEX+1, len(sequence_list)):
+                SECONDARY_SEQUENCE = sequence_list[SECONDARY_INDEX]
+                MATCHING_LETTERS = _matching_letters(PRIMARY_SEQUENCE, SECONDARY_SEQUENCE, COMPARE_INDELS_FLAG)
+
+                if MATCHING_LETTERS > max_matching_letters:
+                    max_matching_letters = MATCHING_LETTERS
+                    max_matching_pair_index = (PRIMARY_INDEX, SECONDARY_INDEX)
+            
         # Necessary condition for popping from list
         assert max_matching_pair_index[0] < max_matching_pair_index[1]
 
