818f35465e51f01e770bb32e2d259c60d674021d
RubenRidderstrom <RubenRidderstrom@users.noreply.github.com>
2018-09-04 21:07:56 +0200

    Optimized match_closest_pairs

--- modelestimator/_calculate_q_eq/match_closest_pair.py
+++ modelestimator/_calculate_q_eq/match_closest_pair.py
@@ -19,29 +19,33 @@ def _matching_letters(a,b, COMPARE_INDELS_FLAG):
 def match_closest_pairs(sequence_list, COMPARE_INDELS_FLAG):
     assert (len(sequence_list) % 2) == 0
 
+    indexes_and_matching_letters = []
+
+    for PRIMARY_INDEX, PRIMARY_SEQUENCE in enumerate(sequence_list):
+        for SECONDARY_INDEX in range(PRIMARY_INDEX+1, len(sequence_list)):
+            SECONDARY_SEQUENCE = sequence_list[SECONDARY_INDEX]
+            MATCHING_LETTERS = _matching_letters(PRIMARY_SEQUENCE, SECONDARY_SEQUENCE, COMPARE_INDELS_FLAG)
+
+            INDEXES = (PRIMARY_INDEX, SECONDARY_INDEX)
+            INDEX_SCORE_TUPLE = (INDEXES, MATCHING_LETTERS)
+            indexes_and_matching_letters.append(INDEX_SCORE_TUPLE)
+
+    indexes_and_matching_letters.sort(key=lambda tup: tup[1], reverse = True)   # Sort on matching letters
+    matched_indexes = []
     closest_pairs = []
+    NUMBER_OF_SEQUENCES = len(sequence_list)
 
-    # Compares every sequence to every other sequence
-    # Puts the two most similar sequences in closest_pairs and then starts over
-    while(len(sequence_list) != 0):
-        max_matching_letters = -1
-        max_matching_pair_index = ()
-    
-        for PRIMARY_INDEX, PRIMARY_SEQUENCE in enumerate(sequence_list):
-            for SECONDARY_INDEX in range (PRIMARY_INDEX+1, len(sequence_list)):
-                SECONDARY_SEQUENCE = sequence_list[SECONDARY_INDEX]
-                MATCHING_LETTERS = _matching_letters(PRIMARY_SEQUENCE, SECONDARY_SEQUENCE, COMPARE_INDELS_FLAG)
+    while len(matched_indexes) != NUMBER_OF_SEQUENCES:
+        CURRENT_INDEX_AND_MATCHING_LETTERS_TUPLE = indexes_and_matching_letters.pop(0)
+        FIRST_INDEX = CURRENT_INDEX_AND_MATCHING_LETTERS_TUPLE[0][1]
+        SECOND_INDEX = CURRENT_INDEX_AND_MATCHING_LETTERS_TUPLE[0][0]
 
-                if MATCHING_LETTERS > max_matching_letters:
-                    max_matching_letters = MATCHING_LETTERS
-                    max_matching_pair_index = (PRIMARY_INDEX, SECONDARY_INDEX)
-            
-        # Necessary condition for popping from list
-        assert max_matching_pair_index[0] < max_matching_pair_index[1]
+        if not(FIRST_INDEX in matched_indexes) and not(SECOND_INDEX in matched_indexes):
+            matched_indexes.append(FIRST_INDEX)
+            matched_indexes.append(SECOND_INDEX)
 
-        SEQUENCE_0 = sequence_list.pop(max_matching_pair_index[1])
-        SEQUENCE_1 = sequence_list.pop(max_matching_pair_index[0])
-        CLOSEST_PAIR = (SEQUENCE_0, SEQUENCE_1)
-        closest_pairs.append(CLOSEST_PAIR)
+            FIRST_SEQUENCE = sequence_list[FIRST_INDEX]
+            SECOND_SEQUENCE = sequence_list[SECOND_INDEX]
+            closest_pairs.append((FIRST_SEQUENCE, SECOND_SEQUENCE))
 
     return closest_pairs
